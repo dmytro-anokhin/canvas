0c7f6a128a0f179b4b362a89e8f568f344a7cb41
Dmytro Anokhin <dmytro.anokhin@gmail.com>
2017-10-30 22:33:23 +0100

    Initial Commit

---
+++ Canvas/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Canvas
+//
+//  Created by Dmytro Anokhin on 30/10/2017.
+//  Copyright © 2017 Dmytro Anokhin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
